f96bbeced950be05eb558bc59a1e7f56e9e619f1
蕉饼 <77569325+BananaCakeComputer@users.noreply.github.com>
2023-09-23 05:45:06 +0800

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,14 @@
 public class Player {
-    public static void main(String[] args) {
-        Hand player = new Hand();
+    public static Hand hand;
+    public Player(){
+        this.hand = new Hand();
+    }
+    public String takeCard(){
+        hand.takeCardFromDeck(Main.roundDeck);
+        //System.out.println(hand.toString());
+        return hand.getCardImg(hand.getHandSize()-1);
+    }
+    public int totalVal(){
+        return hand.calculatedValue();
     }
 }
