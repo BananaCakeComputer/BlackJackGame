0bcc974f2e7bb2088578e2e1c2765270b9bdb316
蕉饼 <77569325+BananaCakeComputer@users.noreply.github.com>
2023-09-23 08:36:08 +0800

    Update Dealer.java

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,7 +1,28 @@
 public class Dealer {
-    //庄家
-    private Object dealer;
+    private Hand dealerHand;
     public Dealer(){
-        dealer = new Object();
+        this.dealerHand = new Hand();
+    }
+    public String takeCard(){
+        dealerHand.takeCardFromDeck(Main.roundDeck);
+        if(dealerHand.getHandSize()==1){
+            return "img/CardDown.png";
+        }else{
+            return dealerHand.getCardImg(dealerHand.getHandSize()-1);
+        }
+    }
+    public int totalVal(){
+        if(dealerHand.getHandSize()<=2){
+            return 0;
+        }else{
+            return dealerHand.calculatedValue();
+        }
+    }
+    public String firstCard(){
+        if(dealerHand.getHandSize()>2){
+            return dealerHand.getCardImg(0);
+        }else{
+            return "img/CardDown.png";
+        }
     }
 }
