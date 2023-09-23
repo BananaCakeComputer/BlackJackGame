afca9cb33e6b1761ff7e6d4e6d4d67b4f5e8fa12
蕉饼 <77569325+BananaCakeComputer@users.noreply.github.com>
2023-09-23 08:36:26 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class Main{
+    public static Deck roundDeck;
     private static int page;
     private static JLabel[] tokenImages;
     public static ArrayList<Integer> roundToken;
@@ -14,6 +16,15 @@ public class Main{
     private static ArrayList<JLabel> tokenLabels;
     public static int balance = 500;
     private static JLabel insertToken;
+    private static JButton[] selections;
+    private static JPanel blackjackTop;
+    private static JPanel blackjack;
+    private static ArrayList<JLabel> playerCardImages;
+    private static Player player = new Player();
+    private static JLabel playerTotal;
+    private static ArrayList<JLabel> dealerCardImages;
+    private static Dealer dealer = new Dealer();
+    private static JLabel dealerTotal;
     public static void mouseEnter(int btnIdx){
         if(page==1){
             tokenImages[btnIdx].setBorder(new LineBorder(new Color(0, 90, 255), 4, true));
@@ -66,7 +77,7 @@ public class Main{
         frame.setSize(800, 500);
 
         //下面板
-        JPanel blackjack = new JPanel();
+        blackjack = new JPanel();
         blackjack.setBounds(-1, 461, 802, 42);
         blackjack.setBackground(new Color(175, 100, 0));
         blackjack.setBorder(new LineBorder(new Color(70, 40, 0)));
@@ -93,7 +104,7 @@ public class Main{
         blackjack.add(ClearBtn);
 
         //上面板
-        JPanel blackjackTop = new JPanel();
+        blackjackTop = new JPanel();
         blackjackTop.setBounds(-1, -1, 802, 42);
         blackjackTop.setBackground(new Color(175, 100, 0));
         blackjackTop.setBorder(new LineBorder(new Color(70, 40, 0)));
@@ -107,7 +118,7 @@ public class Main{
             }
         });
         blackjackTop.add(exit);
-        JButton[] selections = new JButton[6];
+        selections = new JButton[6];
         for(int i = 0; i < selections.length; i++){
             selections[i] = new JButton("▼");
             selections[i].setBounds(115 + i*100, 10, 70, 20);
@@ -151,8 +162,45 @@ public class Main{
         frame.setLayout(null);
         frame.setVisible(true);
     }
+    public static void hit(){
+        playerAddCard();
+        if(player.totalVal()>21){
+            //player输
+            frame.getContentPane().removeAll();
+            frame.repaint();
+        }else if(player.totalVal()==21){
+            //若house的总和不等于21，player赢
+        }
+    }
+    public static void stand(){
+        System.out.println("Stand");
+    }
+    public static void playerAddCard(){
+        playerCardImages.add(new JLabel((new ImageIcon(new ImageIcon(player.takeCard()).getImage().getScaledInstance(100, 145, Image.SCALE_SMOOTH)))));
+        playerCardImages.get(playerCardImages.size()-1).setBounds(-100 + playerCardImages.size()*110, 300, 100, 145);
+        frame.add(playerCardImages.get(playerCardImages.size()-1));
+        playerTotal.setText("Total: " + player.totalVal());
+        playerTotal.setBounds(80, 15, playerTotal.getPreferredSize().width, playerTotal.getPreferredSize().height);
+        frame.revalidate();
+        frame.repaint();
+    }
+    public static void dealerAddCard(){
+        dealerCardImages.add(new JLabel((new ImageIcon(new ImageIcon(dealer.firstCard()).getImage().getScaledInstance(100, 145, Image.SCALE_SMOOTH)))));
+        dealerCardImages.get(dealerCardImages.size()-1).setBounds(-100, 50, 100, 145);
+        frame.add(dealerCardImages.get(dealerCardImages.size()-1));
+        dealerCardImages.add(new JLabel((new ImageIcon(new ImageIcon(dealer.takeCard()).getImage().getScaledInstance(100, 145, Image.SCALE_SMOOTH)))));
+        dealerCardImages.get(dealerCardImages.size()-1).setBounds(-100 + dealerCardImages.size()*110, 50, 100, 145);
+        frame.add(dealerCardImages.get(dealerCardImages.size()-1));
+        if(dealer.totalVal()==0){
+            dealerTotal.setText("Total: ?");
+        }else{
+            dealerTotal.setText("Total: " + dealer.totalVal());
+        }
+        dealerTotal.setBounds(80, 18, dealerTotal.getPreferredSize().width, dealerTotal.getPreferredSize().height);
+        frame.revalidate();
+        frame.repaint();
+    }
     public static void roundStart(){
-        System.out.println("round start");
         page = 2;
         frame.remove(tokenAmountText);
         frame.remove(insertToken);
@@ -162,9 +210,65 @@ public class Main{
         for(int i = 0; i < tokenLabels.size(); i++){
             frame.remove(tokenLabels.get(i));
         }
+        for(int i = 0; i < selections.length; i++){
+            blackjackTop.remove(selections[i]);
+        }
         frame.revalidate();
         frame.repaint();
         tokenLabels = new ArrayList<JLabel>();
+        for(int i = 0; i < roundToken.size(); i++){
+            tokenLabels.add(new JLabel((new ImageIcon(new ImageIcon("token/" + roundToken.get(i) + ".jpg").getImage().getScaledInstance(100, 50, Image.SCALE_SMOOTH)))));
+            tokenLabels.get(i).setBounds(370-(roundToken.size()*40/2)+i*40, 215, 100, 50);
+            /*try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {}*/
+            frame.add(tokenLabels.get(i));
+            /*frame.revalidate();
+            frame.repaint();*/
+        }
+        JLabel houseText = new JLabel("House");
+        houseText.setFont(new Font("", Font.BOLD,20));
+        houseText.setBounds(10, 10, houseText.getPreferredSize().width, houseText.getPreferredSize().height);
+        blackjackTop.add(houseText);
+        JLabel playerText = new JLabel("Player");
+        playerText.setFont(new Font("", Font.BOLD,20));
+        playerText.setBounds(10, 8, playerText.getPreferredSize().width, playerText.getPreferredSize().height);
+        blackjack.add(playerText);
+        JButton hitBtn = new JButton("Hit");
+        hitBtn.setBounds(640, 5, hitBtn.getPreferredSize().width, hitBtn.getPreferredSize().height);
+        hitBtn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                hit();
+            }
+        });
+        blackjack.add(hitBtn);
+        JButton standBtn = new JButton("Stand");
+        standBtn.setBounds(720, 5, standBtn.getPreferredSize().width, standBtn.getPreferredSize().height);
+        blackjack.add(standBtn);
+        standBtn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                stand();
+            }
+        });
+        //将roundDeck设置为新的deck并作为本局游戏的deck
+        try {
+            roundDeck = new Deck();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        //洗牌
+        roundDeck.shuffle();
+        //player取2张牌
+        playerCardImages = new ArrayList<JLabel>();
+        playerTotal = new JLabel("Total: 0");
+        blackjack.add(playerTotal);
+        playerAddCard();
+        playerAddCard();
+        dealerCardImages = new ArrayList<JLabel>();
+        dealerTotal = new JLabel("Total: ?");
+        blackjackTop.add(dealerTotal);
+        dealerAddCard();
+        dealerAddCard();
     }
     public static void main(String[] args) {
         newRound();
